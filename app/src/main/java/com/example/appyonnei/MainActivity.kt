b05f058e800a301e74571b807169ecb17faea7fe
libasse <libasse.g.laye@gmail.com>
2024-02-07 15:27:53 +0100

    first commit

---
+++ app/src/main/java/com/example/appyonnei/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.appyonnei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.bottom_scan -> {
+                    replaceFragment(ScanFragment())
+                    true
+                }
+
+                R.id.bottom_envoyer -> {
+                    replaceFragment(EnvoyerFragment())
+                    true
+                }
+
+                R.id.bottom_folder -> {
+                    replaceFragment(FolderFragment())
+                    true
+                }
+
+                R.id.bottom_menu -> {
+                    replaceFragment(MenuFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+
+
+    }
+    private fun  replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+    }
+}
